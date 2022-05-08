6c48a60178ccd9c2947882264e452a0af9aa8d8f
Iryna Shandar <iryna.shandar@gmail.com>
2022-05-09 00:31:08 +0300

    create register(signup) and login

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require("dotenv").config()
 
 const { DB_HOST } = process.env;
 
+const authRouter = require("./routes/api/auth");
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
@@ -16,7 +17,8 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use("/api/auth", authRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
